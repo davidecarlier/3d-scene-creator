b798e0e54710d5d13223ec64f18861d62275de2d
Davide Carlier <davide.carlier@gmail.com>
2022-10-13 10:57:37 +0200

    update ts configs

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 module.exports = defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: '3d-scene-creator',
-      fileName: (format) => `3d-scene-creator.${format}.js`
+      fileName: (format) => {
+        return format == 'es' ? 'main.js' : `main.${format}.js`
+      }
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['three', 'gsap','three/examples/jsm/controls/OrbitControls'],
+      external: ['three', 'gsap', 'three/examples/jsm/controls/OrbitControls'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           three: 'THREE',
           gsap: 'gsap',
-					'three/examples/jsm/controls/OrbitControls': 'OrbitControls'
+          'three/examples/jsm/controls/OrbitControls': 'OrbitControls'
         }
       }
     }
